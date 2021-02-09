ef3936c4c8384a626b83b0c30bcfa9bd9ed8a901
Cryton Andrew <crytonadrew21@gmail.com>
2021-02-09 23:51:25 +0200

    Error Loading Product Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,11 +3,12 @@ import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap"
 import products from "../products"
 import Rating from  "../components/Rating"
 
-const ProductScreen = ({match}) => {
-    const product = products.find((p) => p._id === match.params.id)
+const ProductScreen = ({ match }) => {
+    const product = products.find({id: match.params.id})
+
     return (
         <>
-           <h1>{product}</h1> 
+           <h1>{product.category}</h1> 
         </>
     )
 }
