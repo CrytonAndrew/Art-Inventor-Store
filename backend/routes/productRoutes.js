1bb6eebe7c064177bc998db25240ed75141992d5
Cryton Andrew <crytonadrew21@gmail.com>
2021-02-15 16:04:01 +0200

    Create a controller, to store all the request functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,10 @@
 import express from "express"
-import asyncHandler from "express-async-handler"
-import Product from "../models/productModel.js"
+import { getProductById, getProducts} from "../controllers/productControllers.js"
 
 const router = express.Router()
 
-// @desc    Fetch all products
-// @route   Get /api/products
-// @access  public
-router.get("/", asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products) 
-}))
+router.route("/").get(getProducts) // Using the controller for all the functionality 
 
-
-// @desc    Fetch single product
-// @route   Get /api/products
-// @access  public
-router.get("/:id",asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    }
-    else {
-        res.status(404)
-        throw new Error("Product not found") // This is gonna go through our error handler
-    }
-    
-}))
+router.route("/:id").get(getProductById)
 
 export default router
