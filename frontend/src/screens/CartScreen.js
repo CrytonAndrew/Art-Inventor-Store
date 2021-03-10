8f1af85bac58a6742937add527ffd9785c8bce50
Cryton Andrew <crytonadrew21@gmail.com>
2021-03-10 19:36:14 +0200

    Update cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -41,7 +41,7 @@ const CartScreen = ({match, location, history}) => {
     // Restructure the screen into a card screen
     return (
         <>
-           <Row>
+           <Row className="order-screen-row">
                <Col md={8}>
                     <h1>Shopping Cart</h1>
                     {cartItems.length === 0 ? <Message>Your cart is empty <Link to="/"> Go back </Link></Message> 
